faef7e90f55eaf3cd85a819b22f8747b519d5b46
avaDLG <100103395+avaDLG@users.noreply.github.com>
2024-03-13 11:27:05 -0500

    Created, connected and implemented detail view

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import TaskList
+from .views import TaskList, TaskDetail
 
 urlpatterns = [
     # needd .as_view urls cannot use class inside it but can use a function
     path('', TaskList.as_view(), name='tasks'),
+
+    # number the tasks by integer values
+    path('task/<int:pk>', TaskDetail.as_view(), name='task'),
 ]
 
--- base/views.py
+++ base/views.py
@@ -1,14 +1,22 @@
-# views.py is what the user sees
-
 from django.shortcuts import render
 #from django.http import HttpResponse
 
 # premade django generic list 
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Task
 
-# render simple view to test
-# inherit ListView
 class TaskList(ListView):
     model = Task 
 
+    # default is object_list
+    # more apt name for objects within class
+    context_object_name = 'tasks'
+
+# returning information about specific task
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+
+    # instead of using task_detail.html
+    template_name = 'base/task.html'
